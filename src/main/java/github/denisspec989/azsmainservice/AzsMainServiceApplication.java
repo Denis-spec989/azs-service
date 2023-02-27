ed7b583a20a335f0dc48d1fe4e84553775d4cfff
Denis Ignatov <dsignatov@licard.com>
2023-02-27 17:47:18 +0300

    Initial commit

---
+++ src/main/java/github/denisspec989/azsmainservice/AzsMainServiceApplication.java
@@ -0,0 +1,13 @@
+package github.denisspec989.azsmainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzsMainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzsMainServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/denisspec989/azsmainservice/AzsMainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.denisspec989.azsmainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzsMainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
