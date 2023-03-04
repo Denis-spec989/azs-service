7fdfb00e9d4d707f97e7c41820e7b40be70028d5
Denis Ignatov <dsignatov@licard.com>
2023-03-04 21:00:57 +0300

    feat: enable discovery

--- src/main/java/github/denisspec989/azsmainservice/AzsMainServiceApplication.java
+++ src/main/java/github/denisspec989/azsmainservice/AzsMainServiceApplication.java
@@ -2,12 +2,14 @@ package github.denisspec989.azsmainservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@EnableEurekaClient
 public class AzsMainServiceApplication {
 
 	public static void main(String[] args) {
